935174035bdfff60a93da75cec9e906b1cbb5a73
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-28 13:30:11 +0500

    CA-1-preview-inserted

--- app/src/main/java/com/example/composeapp/ui/theme/ProfileCard.kt
+++ app/src/main/java/com/example/composeapp/ui/theme/ProfileCard.kt
@@ -1,18 +1,15 @@
 package com.example.composeapp.ui.theme
 
-import android.widget.Button
+import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -27,20 +24,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composeapp.R
 
-@Preview
+@Preview(
+    name = "Light mode",
+    showSystemUi = true,
+    device = Devices.NEXUS_5,
+    showBackground = true
+)
+@Preview(
+    name = "Night mode",
+    group = "Devices",
+    showSystemUi = true,
+    device = Devices.NEXUS_5,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun MainPreview(){
+    ComposeAppTheme{
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            ProfileCard()
+        }
+    }
+}
+
 @Composable
 fun ProfileCard() {
     Card(
